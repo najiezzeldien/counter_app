da56184f5eaf85f0d31f1ab51853935a270d26d2
Naji Ezzeldien <aboalnaja10@gmail.com>
2022-06-05 17:27:57 +0200

    UPd counter app

---
+++ lib/presentation/screens/counter_screen/counter_screen.dart
@@ -0,0 +1,37 @@
+import 'package:counter_app/presentation/screens/counter_screen/widgets/animaded_circles.dart';
+import 'package:counter_app/presentation/screens/counter_screen/widgets/counter_slider.dart';
+import 'package:counter_app/presentation/screens/counter_screen/widgets/counter_value.dart';
+import 'package:counter_app/presentation/screens/counter_screen/widgets/plasma_background.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class CounterScreen extends StatelessWidget {
+  CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          PlasmaBackground(),
+          Positioned(
+            top: 10.0.h,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                AnimatedCircles(),
+                CounterValue(),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 8.0.h,
+            child: CounterSlider(),
+          ),
+        ],
+      ),
+    );
+  }
+}
